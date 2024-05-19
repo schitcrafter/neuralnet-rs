40f7b557172a35022435e8deeb3f3175003c4721
schitcrafter <vapor.schitcrafter@gmail.com>
2024-05-19 14:28:56 +0200

    ✨ Working backpropagation!!!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::error::Error;
 use env_logger::WriteStyle;
 use log::info;
 use nalgebra::DVector;
+use rand::thread_rng;
+
+use rand::seq::SliceRandom;
+use crate::network::{cost_vectors, one_hot_vector};
 
 
 mod data;
@@ -31,19 +35,47 @@ fn main() -> Result<(), Box<dyn Error>> {
     info!("Input dimension: {}", network.input_dimension());
     info!("Output dimension: {}", network.output_dimension());
 
-    for matrix in &network.layer_weights {
-        println!("Matrix dimensions: {:?}", matrix.shape())
-    }
+    let input = training_data.labelled_images.first().unwrap();
+    let input_target_vec = one_hot_vector(input.label);
+    let input_x = DVector::from_vec(input.image.iter().map(|int| *int as f32).collect());
 
-    let input = &training_data.labelled_images.first().unwrap().image;
-    let input = DVector::from_vec(input.iter().map(|int| *int as f32).collect());
+    info!("Forward prop");
+    let output = network.forward_propagation(&input_x);
 
-    let output = network.forward_propagation(input);
-    let output = output.last().unwrap();
-
-    let cost = network::cost_digit(output, training_data.labelled_images.first().unwrap().label);
+    let cost = network::cost_digit(&output, training_data.labelled_images.first().unwrap().label);
 
     info!("cost: {cost}, output: {:?}", output.data.as_slice());
+    
+    info!("Backwards prop");
+    let deltas = network.back_prop_single_input(&input_x, &one_hot_vector(input.label));
+    
+    for delta in &deltas {
+        info!("delta: {:?}", delta.shape());
+        info!("rank: {}", delta.rank(0.1));
+        // info!("{}", delta);
+    }
+
+    info!("Shuffling inputs");
+    let mut labelled_images = training_data.labelled_images;
+    labelled_images.shuffle(&mut thread_rng());
+
+    let backprop_inputs: Vec<_> = labelled_images.iter()
+        .take(labelled_images.len() / 2)
+        .map(|image| (
+            DVector::from_vec(image.image.iter().map(|int| *int as f32).collect()),
+            one_hot_vector(image.label)
+        ))
+        .collect();
+    
+    info!("Starting training on {} inputs!", backprop_inputs.len());
+    let mut network = network;
+    for i in 0..1_000 {
+        let output = network.forward_propagation(&input_x);
+        let cost = cost_vectors(&output, &input_target_vec);
+        info!("[i={i}] cost for first image: {cost}");
+    
+        network.backwards_propagation(&backprop_inputs);
+    }
 
     Ok(())
 }
--- src/network.rs
+++ src/network.rs
@@ -1,12 +1,15 @@
 use itertools::Itertools;
+use log::debug;
 use nalgebra::{DMatrix, DVector};
 use rand::Rng;
+use rayon::prelude::*;
 
 #[derive(Clone)]
 pub struct NeuralNetwork {
     pub layer_sizes: Vec<u32>,
     pub layer_weights: Vec<DMatrix<f32>>,
     pub layer_biases: Vec<DVector<f32>>,
+    pub learning_rate: f32,
 }
 
 impl NeuralNetwork {
@@ -30,12 +33,8 @@ impl NeuralNetwork {
         assert_eq!(layer_weights[0].nrows(), layer_weights[1].ncols());
 
         let mut layer_biases = Vec::new();
-
         for layer_size in &layer_sizes[1..] {
-            let new_vector = DVector::from_fn(*layer_size as usize,
-                |_, _| rng.gen_range(-1.0..=0.0));
-            
-            layer_biases.push(new_vector);
+            layer_biases.push(DVector::<f32>::zeros(*layer_size as usize));
         }
 
         assert_eq!(layer_biases.len(), layer_sizes.len() - 1);
@@ -43,36 +42,122 @@ impl NeuralNetwork {
         NeuralNetwork {
             layer_sizes,
             layer_weights,
-            layer_biases
+            layer_biases,
+            learning_rate: 0.1
         }
     }
 
-    /// Returns the intermediate states of all neurons.
+    /// Returns the activation of all neurons, plus the derivative of the activation
+    /// function evaluated with the activation.
     /// The first element will be the input, the last element the output.
-    pub fn forward_propagation(&self, input: DVector<f32>) -> Vec<DVector<f32>> {
-        let mut intermediate_outputs = vec![input];
+    pub fn forward_propagation(&self, input: &DVector<f32>) -> DVector<f32> {
+        let mut last_output = input.clone();
 
         for (weights, biases) in self.layer_weights.iter().zip(&self.layer_biases) {
-            let input = intermediate_outputs.last().unwrap();
+            let mut activation = weights * last_output;
+            activation += biases;
 
-            let mut output = weights * input;
-            output += biases;
-            output.apply(|value| *value = sigmoid(*value));
+            activation.apply(|value| *value = sigmoid(*value));
 
-            intermediate_outputs.push(output);
+            last_output = activation;
         }
 
-        intermediate_outputs
+        last_output
     }
-
-    pub fn back_prop(&mut self, _inputs: Vec<DVector<f32>>) {
-
-    }
-
+    
     /// Does backpropagation for a single input, returning
     /// the wanted changes to the weights.
-    pub fn back_prop_single_input(&self, _input: DVector<f32>) -> Vec<DMatrix<f32>> {
-        vec![]
+    pub fn back_prop_single_input(&self, input: &DVector<f32>, y: &DVector<f32>) -> Vec<DMatrix<f32>> {
+        // do forward prop and get all information out of it that we can
+        let mut activations = vec![input.clone()];
+        
+        for (weights, biases) in self.layer_weights.iter().zip(&self.layer_biases) {
+            let last_output = activations.last().unwrap();
+            let mut activation = weights * last_output;
+            activation += biases;
+
+            activation.apply(|value| *value = sigmoid(*value));
+
+            activations.push(activation);
+        }
+
+        // This will store the gradients all weights will go to
+        let mut weight_gradients = vec![];
+
+        let output_activ = activations.last().unwrap();
+        let error_deriv = output_activ - y;
+        let mut last_delta = error_deriv.component_mul(&output_activ.map(sigmoid_derivative));
+
+        let last_layer_weight_gradient = &last_delta * output_activ.transpose();
+        weight_gradients.push(last_layer_weight_gradient);
+
+        let weight_activation_iter =
+            self.layer_weights.iter().rev()
+            .zip(activations.into_iter().rev().tuple_windows());
+
+        for (weights, (activation_right, activation_left)) in weight_activation_iter {
+            debug!("weights: {:?}, activation_left: {:?}, activation_right: {:?}", weights.shape(), activation_left.shape(), activation_right.shape());
+            debug!("last_delta: {:?}", last_delta.data.as_slice());
+
+            let weight_gradient = &last_delta * activation_left.transpose();
+            assert_eq!(weight_gradient.shape(), weights.shape(), "Shape of weight gradient matrix is wrong");
+            weight_gradients.push(weight_gradient);
+            
+            let new_delta = weights.transpose() * last_delta;
+            debug!("W^T * last_delta: {:?}", new_delta.data.as_slice());
+            let activation_deriv = activation_left.map(sigmoid_derivative);
+            debug!("activation_deriv: {:?}", activation_deriv.data.as_slice());
+
+            // left: (10, 1) right: (16, 1)
+            // new_delta neeeds to be (10, 1) for this to work
+            let new_delta = activation_deriv.component_mul(&new_delta);
+            debug!("new_delta: {:?}", new_delta.data.as_slice());
+
+            last_delta = new_delta;
+        }
+
+        weight_gradients.reverse();
+
+        for (weight_gradient, weight) in weight_gradients.iter().zip(&self.layer_weights) {
+            assert_eq!(weight_gradient.shape(), weight.shape());
+        }
+
+        weight_gradients
+    }
+
+    /// inputs is a list of input vectors to expected outputs
+    pub fn backwards_propagation(&mut self, inputs: &[(DVector<f32>, DVector<f32>)]) {
+        let mut changes: Vec<DMatrix<f32>> = Vec::new();
+
+        for weight_mat in &self.layer_weights {
+            let zero_mat = DMatrix::zeros(weight_mat.nrows(), weight_mat.ncols());
+            changes.push(zero_mat);
+        }
+
+        let mut zero_matrices = Vec::new();
+        for weight in &self.layer_weights {
+            zero_matrices.push(DMatrix::zeros(weight.nrows(), weight.ncols()));
+        }
+
+        let added_deltas = inputs.par_iter()
+            .map(|(input, target)|
+                self.back_prop_single_input(input, target))
+            .reduce(|| zero_matrices.clone(), |acc, deltas| {
+                acc.into_iter().zip(deltas)
+                    .map(|(l, r)| l + r)
+                    .collect()
+            });
+
+        let input_length = inputs.len() as f32;
+        let added_deltas: Vec<_> = added_deltas.into_iter()
+            .map(|weight_delta| (weight_delta * self.learning_rate) / input_length)
+            .collect();
+
+        self.layer_weights = self.layer_weights.iter()
+            .zip(added_deltas)
+            .map(|(weights, deltas)| weights - deltas)
+            .collect();
+        
     }
 
     pub fn input_dimension(&self) -> u32 {
@@ -85,7 +170,7 @@ impl NeuralNetwork {
 }
 
 pub fn cost_digit(output: &DVector<f32>, digit: u8) -> f32 {
-    let expected = expected_output(digit);
+    let expected = one_hot_vector(digit);
     cost_vectors(output, &expected)
 }
 
@@ -99,13 +184,19 @@ pub fn cost_vectors(output: &DVector<f32>, expected_output: &DVector<f32>) -> f3
     sum * 0.5
 }
 
-pub fn expected_output(digit: u8) -> DVector<f32> {
+pub fn one_hot_vector(digit: u8) -> DVector<f32> {
     let mut vec = vec![0f32; 10];
     vec[digit as usize] = 1.0;
 
     DVector::from_vec(vec)
 }
 
-fn sigmoid(z: f32) -> f32 {
-    1.0 / (1.0 + std::f32::consts::E.powf(-z))
+fn sigmoid(x: f32) -> f32 {
+    1.0 / (1.0 + std::f32::consts::E.powf(-x))
+}
+
+/// takes the derivative of the sigmoid, taking the output
+/// of the sigmoid as output.
+fn sigmoid_derivative(x: f32) -> f32 {
+    x * (1.0 - x)
 }
